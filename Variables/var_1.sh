c6a54ad9552033051d41ba8aced0af7c8b898c2d
onlinetraining2020 <72064253+onlinetraining2020@users.noreply.github.com>
2022-01-18 19:54:10 +0530

    Variables

---
+++ Variables/var_1.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# variable assignment and printing
+
+MYSTRVAR="This is a string value assigned" #no space before and after = assignment operator
+MYINTVAR=5
+
+echo "Printing the MYSTRVAR - $MYSTRVAR"
+echo "Printing the MYINTVAR - $MYINTVAR"
+
